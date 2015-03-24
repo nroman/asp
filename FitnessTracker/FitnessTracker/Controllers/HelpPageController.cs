da4c71221f2a477f6138f2d9dd9f2ac669701f09
nroman <nebeliuk.roman@gmail.com>
2015-03-24 16:48:14 -0400

    A few small modifications in the styles. Added two new models for the db tables. One main model for the exercise whitch contains a list of the sets of the second model. Added updated dlles for the entity framework package. Added new simple help page with body parts image.

---
+++ FitnessTracker/FitnessTracker/Controllers/HelpPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitnessTracker.Controllers
+{
+    public class HelpPageController : Controller
+    {
+        // GET: HelpPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FitnessTracker/FitnessTracker/Models/Exercise.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitnessTracker.Models
+{
+    public class Exercise
+    {
+        public int ID { get; set; }
+
+        public String Name { get; set; }
+
+        public BodyPart CurrentBodyPart { get; set; }
+
+        public MeasurementSystem Measurement { get; set; }
+
+        public virtual List<ExerciseSet> ExerciseSet { get; set; }
+    }
+
+    public class ExerciseSet
+    {
+        public Double Weight { get; set; }
+
+        public Double MovementsCount { get; set; }
+            
+        public virtual Exercise Exercise { get; set; }
+    }
+
+    /// <summary>
+    /// The parts of the body
+    /// </summary>
+    public enum BodyPart
+    {
+        Neck = 0,
+        Traps,
+        Shoulders,
+        Lats,
+        MidleBack,
+        LowerBack,        
+        Chest,
+        Abs,
+        Biceps,
+        Triceps,       
+        Forearm,        
+        Gluts,
+        Quads,
+        Hamstrings,
+        Calves
+    }
+
+    public enum MeasurementSystem
+    {
+        Metric = 0,
+        English
+    }
+}
